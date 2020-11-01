1375f2c8a83200970c3a9d157c4bd15adf82bc63
Dipak_Pagare <dipakpagare@github.com>
2020-11-01 23:14:21 +0530

    Updates

---
+++ precache-manifest.80a670a8dd5f2df6b485d8754ffd6a09.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ccc339670abbb849401a2f9fe032fa",
+    "url": "/ShopIndia/index.html"
+  },
+  {
+    "revision": "f4d1a3a0f5341c10b46a",
+    "url": "/ShopIndia/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "b402436e0c677f7fa8bd",
+    "url": "/ShopIndia/static/css/main.42005222.chunk.css"
+  },
+  {
+    "revision": "f4d1a3a0f5341c10b46a",
+    "url": "/ShopIndia/static/js/2.588b845b.chunk.js"
+  },
+  {
+    "revision": "784555e1d413f001dbd00f00741afbcf",
+    "url": "/ShopIndia/static/js/2.588b845b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b402436e0c677f7fa8bd",
+    "url": "/ShopIndia/static/js/main.15de92a1.chunk.js"
+  },
+  {
+    "revision": "65c9f8058526f1ba0ba2",
+    "url": "/ShopIndia/static/js/runtime-main.1d5c960a.js"
+  },
+  {
+    "revision": "4179959e16a989dbb7927c29ef20674c",
+    "url": "/ShopIndia/static/media/Indianterrain.4179959e.jpg"
+  },
+  {
+    "revision": "ebeda5740eb654794dc059dc8128de64",
+    "url": "/ShopIndia/static/media/Raymond.ebeda574.jpg"
+  },
+  {
+    "revision": "32cc72023e8ff8a7e519f3ac34f829a1",
+    "url": "/ShopIndia/static/media/anokhi.32cc7202.jpg"
+  },
+  {
+    "revision": "dec12cb1178595b35a5a083ca689e45f",
+    "url": "/ShopIndia/static/media/bewakoof.dec12cb1.jpg"
+  },
+  {
+    "revision": "582acceb30fa98c3da4d5e74f80e084d",
+    "url": "/ShopIndia/static/media/fabindia.582acceb.jpg"
+  },
+  {
+    "revision": "0217abd59193b3fb64a375ba594a87f9",
+    "url": "/ShopIndia/static/media/flyingmachine.0217abd5.jpg"
+  },
+  {
+    "revision": "0ac5cc725b6ab1323109b77c3cd857d2",
+    "url": "/ShopIndia/static/media/garden-vareli.0ac5cc72.jpg"
+  },
+  {
+    "revision": "db361046efef1bfce07d37036e302741",
+    "url": "/ShopIndia/static/media/ginger.db361046.jpg"
+  },
+  {
+    "revision": "00f9634769f7ebf7590a8f0a537927c1",
+    "url": "/ShopIndia/static/media/globaldesi.00f96347.png"
+  },
+  {
+    "revision": "1057d63a6582a9ca418ccd0e122637c1",
+    "url": "/ShopIndia/static/media/green teas.1057d63a.jpg"
+  },
+  {
+    "revision": "a1d7624839ab13b211e629fb01fd8ead",
+    "url": "/ShopIndia/static/media/hrxby.a1d76248.jpg"
+  },
+  {
+    "revision": "a8fb6dd87fe9a0cad6b60f7eb4bcfef4",
+    "url": "/ShopIndia/static/media/indiaflag.a8fb6dd8.png"
+  },
+  {
+    "revision": "5dfd3a17b88e068f408171b554452770",
+    "url": "/ShopIndia/static/media/kappa.5dfd3a17.jpg"
+  },
+  {
+    "revision": "9149cde764d180ef4ca1721c8af1259e",
+    "url": "/ShopIndia/static/media/kforkathiyavad.9149cde7.jpg"
+  },
+  {
+    "revision": "f1e8b05af21e72504a762e0dbcfe9d87",
+    "url": "/ShopIndia/static/media/lyfestyle.f1e8b05a.jpg"
+  },
+  {
+    "revision": "ff67a057937106e9655ffbc43e3442c7",
+    "url": "/ShopIndia/static/media/madame.ff67a057.jpg"
+  },
+  {
+    "revision": "18124a9f173469398ceeeab671a29538",
+    "url": "/ShopIndia/static/media/manyavar.18124a9f.jpg"
+  },
+  {
+    "revision": "81018f648706aba95e08b6b8eb93db80",
+    "url": "/ShopIndia/static/media/modarapido.81018f64.jpg"
+  },
+  {
+    "revision": "c2a4f744f8113542ce412cec5411669e",
+    "url": "/ShopIndia/static/media/montecarlo.c2a4f744.jpg"
+  },
+  {
+    "revision": "584ac98e5e3ffd0eb2607aacc565870e",
+    "url": "/ShopIndia/static/media/mufti.584ac98e.jpg"
+  },
+  {
+    "revision": "7fc9aec0086e40c3f9b961003a7efbb6",
+    "url": "/ShopIndia/static/media/nalli.7fc9aec0.jpg"
+  },
+  {
+    "revision": "c1e05f37622a524cdce5a92fcfe196c4",
+    "url": "/ShopIndia/static/media/park avenue.c1e05f37.jpg"
+  },
+  {
+    "revision": "014f20bf654fb8e6e297e667ed8fb644",
+    "url": "/ShopIndia/static/media/raymondcostom.014f20bf.jpg"
+  },
+  {
+    "revision": "ed2d13f6b587cd2fde9038e1445ddd21",
+    "url": "/ShopIndia/static/media/raymondnext.ed2d13f6.jpg"
+  },
+  {
+    "revision": "f5d0633c587656270fc025e11902a856",
+    "url": "/ShopIndia/static/media/spykar.f5d0633c.jpg"
+  },
+  {
+    "revision": "322cf51c8a7577e2b467f9b9a327fc4a",
+    "url": "/ShopIndia/static/media/van.322cf51c.jpg"
+  },
+  {
+    "revision": "2f36427179a68dac81e64e428c60dd36",
+    "url": "/ShopIndia/static/media/vimal.2f364271.jpg"
+  },
+  {
+    "revision": "17d6ef295b6e46a6015466f23fd7058d",
+    "url": "/ShopIndia/static/media/westside.17d6ef29.jpg"
+  },
+  {
+    "revision": "e076082cc68a921411b91acbcc5a577a",
+    "url": "/ShopIndia/static/media/wrogn.e076082c.jpg"
+  }
+]);
